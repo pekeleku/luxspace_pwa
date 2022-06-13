0e3317930b5b1cdefd6b44a22b4d31e2a7c16db4
pekeleku <alumanwalter@gmail.com>
2022-06-13 19:23:41 +0800

    make <g/> to component

---
+++ src/components/CircleNotif.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CircleNotif() {
+  return (
+    <g className="text-pink-400">
+      <circle cx="25" cy="4.89023" r="4" className="fill-current dot" />
+    </g>
+  );
+}
